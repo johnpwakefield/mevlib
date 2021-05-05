9ae01a77191ab0dbf43081a2eb9112fd23ab0eac
John P Wakefield <jwake@umich.edu>
2021-05-04 22:10:11 -0400

    made test outline

---
+++ mevlib/scripts/run_fullcircle_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+
+
+
+# run make_table
+
+
+
+# compile mevlookup.mod
+
+
+# compile fullcircletest (and link mevlookup)
+
+
+
+# test mesh values and compute interpolation error
+
+
+
+
+#TODO delete this when draft of test is complete
+assert(False)
+
+
