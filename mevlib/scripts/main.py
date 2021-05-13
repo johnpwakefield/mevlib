b364aa873f499dd0f0552892d67d1aa98f5acaf2
John P Wakefield <jwake@umich.edu>
2021-05-13 14:55:09 -0400

    formatting fix

--- mevlib/scripts/main.py
+++ mevlib/scripts/main.py
@@ -5,20 +5,12 @@ import click
 from mevlib.maketable import make_table
 
 
-
-
-
-
-
 def copystatement():
     click.echo("""
 MEVLIB  Copyright (C) 2021  J Wakefield, A Lattanzi, J Capecelatro
 This program comes with ABSOLUTELY NO WARRANTY.  This is free software, and
 you are welcome to redistribute it under the GNU General Public License.
     """)
-
-
-
 
 
 @click.command()
@@ -28,8 +20,5 @@ you are welcome to redistribute it under the GNU General Public License.
 def maketable(src, dst, discard_products=False):
     copystatement()
     make_table(src, dst, True, not discard_products)
-
-
-
 
 
