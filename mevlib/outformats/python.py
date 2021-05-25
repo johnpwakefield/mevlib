52f9b37c2828f31c76128ae00bf844fbad142681
John P Wakefield <jwake@umich.edu>
2021-05-25 09:26:33 -0400

    added pickle outformat; have not yet added wrappers

--- mevlib/outformats/python.py
+++ mevlib/outformats/python.py
@@ -1,8 +1,11 @@
 
 
+from pickle import dump
+
+
 def pickle(outfile, matrices, temperatures, verb=False):
     print("Generating a pickle file for use with 'mevlookup.py'...")
     with open(outfile, 'wb') as f:
-        pickle.dumps(f, {'mats': matrices, 'temps': temperatures})
+        dump({'mats': matrices, 'temps': temperatures}, f)
 
 
